492d8b88992b2802456c1ea333df3cdd0231d3dd
gujiu6 <18273655445@163.com>
2026-07-24 12:28:10 +0800

    11

---
+++ CCPC/Province/LNCPC/2023/M.让二追三.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+//#include <ranges>
+using namespace std;
+#define endl '\n'
+#define all(A,i) A.begin()+i, A.end()
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define per(i,r,l) for(int i=r;i>=l;i--)
+#define pb push_back
+#define dbg(x) cout<<#x<<"="<<x<<endl;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using cd = complex<double>;
+using pii = array<int, 2>;
+const int INF=1e9+7;
+const int MOD=1e9+7;
+const int MAXX=2e5+10,LIMIT=20;
+const double eps=1e-6;
+const double PI = acos(-1);
+int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
+int dx8[]={0,0,1,1,1,-1,-1,-1}, dy8[]={1,-1,-1,0,1,-1,0,1};
+
+
+ll power(ll a, ll b) {
+	ll ans = 1;
+	a %= MOD;
+	while(b) {
+		if(b & 1) ans = (ans * a) % MOD;
+		b >>= 1;
+		a = (a * a) % MOD;
+	}
+	return ans;
+}
+
+ll get_inv(ll x) {
+	return power(x, MOD - 2);
+}
+
+inline void solve() {
+	ll a, b, n; cin >> a >> b >> n;
+	if(n < 5) {
+		cout << 0 << endl;
+		return;
+	}
+	ll c = b - a;
+	ll p = a * get_inv(b);
+	ll q = (b - a) * get_inv(b);
+	ll e = 1LL * power(q, 2) % MOD * power(p, 3) % MOD;
+	cout << (e * (n - 4) % MOD + MOD) % MOD << endl;
+	
+}
+
+
+
+
+
+
+
+signed main() {
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+	int t=1;
+	cin>>t;
+	while(t--){
+	
+		solve();
+	
+	}
+	return 0;
+}
+// 难道我不配AC吗
+//   ▄████  █    ██  ▄▄▄██▀▀▀██▓ █    ██
+//  ██▒ ▀█▒ ██  ▓██▒   ▒██  ▓██▒ ██  ▓██▒
+// ▒██░▄▄▄░▓██  ▒██░   ░██  ▒██▒▓██  ▒██░
+// ░▓█  ██▓▓▓█  ░██░▓██▄██▓ ░██░▓▓█  ░██░
+// ░▒▓███▀▒▒▒█████▓  ▓███▒  ░██░▒▒█████▓
+//  ░▒   ▒ ░▒▓▒ ▒ ▒  ▒▓▒▒░  ░▓  ░▒▓▒ ▒ ▒
+//   ░   ░ ░░▒░ ░ ░  ▒ ░▒░   ▒ ░░░▒░ ░ ░
+//  ░   ░  ░░░ ░ ░  ░ ░ ░   ▒ ░ ░░░ ░ ░
+//       ░    ░      ░   ░   ░     ░
