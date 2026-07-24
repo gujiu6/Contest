999c39eb4594a57308ffc5dd17ce53b93d0f6e8e
gujiu6 <18273655445@163.com>
2026-07-24 20:38:17 +0800

    11

---
+++ CCPC/Province/LNCPC/2022/M.画画.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+//#include <ranges>
+using namespace std;
+#define endl '\n'
+#define all(A,i) A.begin()+i, A.end()
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define per(i,r,l) for(int i=r;i>=l;i--)
+#define pb push_back
+#define dbg(x) cout<<#x<<"="<<x<<endl;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using cd = complex<double>;
+using pii = array<int, 2>;
+const int INF=1e9+7;
+const int MOD=1e9+7;
+const int MAXX=2e5+10,LIMIT=20;
+const double eps=1e-6;
+const double PI = acos(-1);
+int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
+int dx8[]={0,0,1,1,1,-1,-1,-1}, dy8[]={1,-1,-1,0,1,-1,0,1};
+
+
+
+
+
+
+inline void solve() {
+	int n; cin >> n;
+	int m = n / 2;
+	rep(i, 0, n - 1) {
+		rep(j, 0, n - 1) {
+			if(i < n - 1 && j < n - 1) cout << 1;
+			else {
+				if((i % 2 == n % 2 || j % 2 == n % 2) || (i == n - 1 && j == n - 1 && (m % 2 != (n - 1) % 2))) cout << 1 ;
+				else cout << 0;
+			}
+		} 
+		cout << endl;
+	}
+	
+}
+
+
+
+
+
+
+
+signed main() {
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+	int t=1;
+	cin>>t;
+	while(t--){
+	
+		solve();
+	
+	}
+	return 0;
+}
+// 难道我不配AC吗
+//   ▄████  █    ██  ▄▄▄██▀▀▀██▓ █    ██
+//  ██▒ ▀█▒ ██  ▓██▒   ▒██  ▓██▒ ██  ▓██▒
+// ▒██░▄▄▄░▓██  ▒██░   ░██  ▒██▒▓██  ▒██░
+// ░▓█  ██▓▓▓█  ░██░▓██▄██▓ ░██░▓▓█  ░██░
+// ░▒▓███▀▒▒▒█████▓  ▓███▒  ░██░▒▒█████▓
+//  ░▒   ▒ ░▒▓▒ ▒ ▒  ▒▓▒▒░  ░▓  ░▒▓▒ ▒ ▒
+//   ░   ░ ░░▒░ ░ ░  ▒ ░▒░   ▒ ░░░▒░ ░ ░
+//  ░   ░  ░░░ ░ ░  ░ ░ ░   ▒ ░ ░░░ ░ ░
+//       ░    ░      ░   ░   ░     ░
