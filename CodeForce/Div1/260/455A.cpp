a25f939fab7774cd1d9f0ff6b2345196c2387072
gujiu6 <18273655445@163.com>
2026-07-28 21:41:57 +0800

    111

---
+++ CodeForce/Div1/260/455A.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+//#include <ranges>
+using namespace std;
+#define endl '\n'
+#define all(A,i) A.begin()+i, A.end()
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define per(i,r,l) for(int i=r;i>=l;i--)
+#define pb push_back
+#define dbg(x) cout<<#x<<"="<<x<<endl;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using cd = complex<double>;
+using pii = array<int, 2>;
+const ll INF = 1e18;
+const int MOD = 1e9+7;
+const int MAXX = 1e5+10, LIMIT = 20;
+const double eps = 1e-6, PI = acos(-1);
+int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
+int dx8[]={0,0,1,1,1,-1,-1,-1}, dy8[]={1,-1,-1,0,1,-1,0,1};
+
+
+
+
+
+
+inline void solve() {
+    int n; cin >> n;
+    int mx = 0;
+    vector<ll> vis(MAXX), dp(MAXX);
+    rep(i, 1, n) {
+        int c; cin >> c;
+        mx = max(mx, c);
+        vis[c]++;
+    }
+    dp[1] = vis[1];
+    dp[2] = max(vis[1], 2 * vis[2]);
+    ll ans = 0;
+    for(int i = 3; i <= mx; i++) {
+        dp[i] = max(dp[i - 1], dp[i - 2] + i * vis[i]);
+    }
+    cout << dp[mx] << endl;
+    
+}
+
+
+
+
+
+
+
+signed main() {
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+    int t=1;
+    //cin>>t;
+    while(t--){
+    
+        solve();
+    
+    }
+    return 0;
+}
+// 难道我不配AC吗
+//   ▄████  █    ██  ▄▄▄██▀▀▀██▓ █    ██
+//  ██▒ ▀█▒ ██  ▓██▒   ▒██  ▓██▒ ██  ▓██▒
+// ▒██░▄▄▄░▓██  ▒██░   ░██  ▒██▒▓██  ▒██░
+// ░▓█  ██▓▓▓█  ░██░▓██▄██▓ ░██░▓▓█  ░██░
+// ░▒▓███▀▒▒▒█████▓  ▓███▒  ░██░▒▒█████▓
+//  ░▒   ▒ ░▒▓▒ ▒ ▒  ▒▓▒▒░  ░▓  ░▒▓▒ ▒ ▒
+//   ░   ░ ░░▒░ ░ ░  ▒ ░▒░   ▒ ░░░▒░ ░ ░
+//  ░   ░  ░░░ ░ ░  ░ ░ ░   ▒ ░ ░░░ ░ ░
+//       ░    ░      ░   ░   ░     ░
