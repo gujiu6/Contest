a662b6c93385ca43bc4ef24d1baa3b55ecfbd972
gujiu6 <18273655445@163.com>
2026-07-29 13:53:20 +0800

    111

---
+++ CodeForce/Edu/63/1155D.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+//#include <ranges>
+using namespace std;
+#define endl '\n'
+#define all(A,i) A.begin()+i, A.end()
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define per(i,r,l) for(int i=r;i>=l;i--)
+#define pb push_back
+#define dbg(x) cout<<#x<<"="<<x<<endl;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using cd = complex<double>;
+using pii = array<int, 2>;
+const ll INF = 1e18;
+const int MOD = 1e9+7;
+const int MAXX = 2e5+10, LIMIT = 20;
+const double eps = 1e-6, PI = acos(-1);
+int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
+int dx8[]={0,0,1,1,1,-1,-1,-1}, dy8[]={1,-1,-1,0,1,-1,0,1};
+
+
+
+
+
+
+inline void solve() {
+    int n, x; cin >> n >> x;
+    ll dp1 = -INF, dp2 = -INF, dp3 = -INF, ans = 0;
+    rep(i, 1, n) {
+        ll c; cin >> c;
+        ll ddp1 = 0, ddp2 = 0, ddp3 = 0;
+        ddp1 = max(dp1 + c, c);
+        ddp2 = max({dp2 + c * x, dp1 + c * x, c * x});
+        ddp3 = max(dp3 + c, dp2 + c);
+        swap(dp1, ddp1);
+        swap(dp2, ddp2);
+        swap(dp3, ddp3);
+        ans = max({ans, dp1, dp2, dp3});
+    }
+    cout << ans << endl;
+}
+
+
+
+
+
+
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    //cin >> t;
+    while(t--){
+    
+        solve();
+    
+    }
+    return 0;
+}
+// 难道我不配AC吗
+//   ▄████  █    ██  ▄▄▄██▀▀▀██▓ █    ██
+//  ██▒ ▀█▒ ██  ▓██▒   ▒██  ▓██▒ ██  ▓██▒
+// ▒██░▄▄▄░▓██  ▒██░   ░██  ▒██▒▓██  ▒██░
+// ░▓█  ██▓▓▓█  ░██░▓██▄██▓ ░██░▓▓█  ░██░
+// ░▒▓███▀▒▒▒█████▓  ▓███▒  ░██░▒▒█████▓
+//  ░▒   ▒ ░▒▓▒ ▒ ▒  ▒▓▒▒░  ░▓  ░▒▓▒ ▒ ▒
+//   ░   ░ ░░▒░ ░ ░  ▒ ░▒░   ▒ ░░░▒░ ░ ░
+//  ░   ░  ░░░ ░ ░  ░ ░ ░   ▒ ░ ░░░ ░ ░
+//       ░    ░      ░   ░   ░     ░
