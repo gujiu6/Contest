3fcc255179234c98823c08bf4e8dc3588ff04190
gujiu6 <18273655445@163.com>
2026-07-29 16:38:01 +0800

    11

---
+++ CodeForce/Edu/86/1342C.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+//#include <ranges>
+using namespace std;
+#define endl '\n'
+#define all(A,i) A.begin()+i, A.end()
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define per(i,r,l) for(int i=r;i>=l;i--)
+#define pb push_back
+#define dbg(x) cout<<#x<<"="<<x<<endl;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using cd = complex<double>;
+using pii = array<int, 2>;
+const ll INF = 1e18;
+const int MOD = 1e9+7;
+const int MAXX = 2e5+10, LIMIT = 20;
+const double eps = 1e-6, PI = acos(-1);
+int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
+int dx8[]={0,0,1,1,1,-1,-1,-1}, dy8[]={1,-1,-1,0,1,-1,0,1};
+
+int a, b, q;
+int L;
+ll query(ll i)
+{
+    if(i < 0) return 0;
+    ll mx = max(a,b);
+    ll cnt = i / L;
+    ll rem = i % L;
+    return cnt * mx + min(rem + 1, mx);
+}
+
+ll query(ll l, ll r) {
+    return query(r) - query(l - 1);
+}
+
+
+inline void solve() {
+    cin >> a >> b >> q;
+    L = lcm(a, b);
+    // a = 4, b = 6;
+    // for(int i = 0; i <= 100; i++) {
+    //     if(i % a % b == i % b % a) {
+    //         cout << i << endl;
+    //     }
+    // }
+    while(q--) {
+        ll l, r; cin >> l >> r;
+        cout << (r - l + 1) - query(l, r) << " " ;
+    }
+    cout << endl;
+    // cout << query(9);
+}
+
+
+
+
+
+
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while(t--){
+    
+        solve();
+    
+    }
+    return 0;
+}
+// 难道我不配AC吗
+//   ▄████  █    ██  ▄▄▄██▀▀▀██▓ █    ██
+//  ██▒ ▀█▒ ██  ▓██▒   ▒██  ▓██▒ ██  ▓██▒
+// ▒██░▄▄▄░▓██  ▒██░   ░██  ▒██▒▓██  ▒██░
+// ░▓█  ██▓▓▓█  ░██░▓██▄██▓ ░██░▓▓█  ░██░
+// ░▒▓███▀▒▒▒█████▓  ▓███▒  ░██░▒▒█████▓
+//  ░▒   ▒ ░▒▓▒ ▒ ▒  ▒▓▒▒░  ░▓  ░▒▓▒ ▒ ▒
+//   ░   ░ ░░▒░ ░ ░  ▒ ░▒░   ▒ ░░░▒░ ░ ░
+//  ░   ░  ░░░ ░ ░  ░ ░ ░   ▒ ░ ░░░ ░ ░
+//       ░    ░      ░   ░   ░     ░
