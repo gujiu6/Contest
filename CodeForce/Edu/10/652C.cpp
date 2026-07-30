3d7c8425961b3a8c33a0b633d59dd77cd4ebeceb
gujiu6 <18273655445@163.com>
2026-07-30 19:25:41 +0800

    111

---
+++ CodeForce/Edu/10/652C.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+//#include <ranges>
+using namespace std;
+#define endl '\n'
+#define all(A,i) A.begin()+i, A.end()
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define per(i,r,l) for(int i=r;i>=l;i--)
+#define pb push_back
+#define dbg(x) cout<<#x<<"="<<x<<endl;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using cd = complex<double>;
+using pii = array<int, 2>;
+const ll INF = 1e18;
+const int MOD = 1e9+7;
+const int MAXX = 2e5+10, LIMIT = 20;
+const double eps = 1e-6, PI = acos(-1);
+int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
+int dx8[]={0,0,1,1,1,-1,-1,-1}, dy8[]={1,-1,-1,0,1,-1,0,1};
+
+
+
+
+
+
+inline void solve() {
+    int n, m; cin >> n >> m;
+    vector<int> a(n + 1);
+    vector<int> pos(n + 1);
+    vector<int> max_L(n + 1, 0), limit(n + 1, 0);
+    rep(i, 1, n) cin >> a[i], pos[a[i]] = i;
+    ll ans = 0;
+    rep(i, 1, m) {
+        int u, v; cin >> u >> v;
+        int pos_l = min(pos[u], pos[v]), pos_r = max(pos[u], pos[v]);
+        max_L[pos_r] = max(max_L[pos_r], pos_l);
+    }
+    for(int r = 1; r <= n; r++) {
+        limit[r] = max(max_L[r], limit[r - 1]);
+        ans += (r - limit[r]);
+        // dbg(r)dbg(limit[r])
+    }
+    cout << ans << endl;
+}
+
+
+
+
+
+
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    //cin >> t;
+    while(t--){
+    
+        solve();
+    
+    }
+    return 0;
+}
+// 难道我不配AC吗
+//   ▄████  █    ██  ▄▄▄██▀▀▀██▓ █    ██
+//  ██▒ ▀█▒ ██  ▓██▒   ▒██  ▓██▒ ██  ▓██▒
+// ▒██░▄▄▄░▓██  ▒██░   ░██  ▒██▒▓██  ▒██░
+// ░▓█  ██▓▓▓█  ░██░▓██▄██▓ ░██░▓▓█  ░██░
+// ░▒▓███▀▒▒▒█████▓  ▓███▒  ░██░▒▒█████▓
+//  ░▒   ▒ ░▒▓▒ ▒ ▒  ▒▓▒▒░  ░▓  ░▒▓▒ ▒ ▒
+//   ░   ░ ░░▒░ ░ ░  ▒ ░▒░   ▒ ░░░▒░ ░ ░
+//  ░   ░  ░░░ ░ ░  ░ ░ ░   ▒ ░ ░░░ ░ ░
+//       ░    ░      ░   ░   ░     ░
