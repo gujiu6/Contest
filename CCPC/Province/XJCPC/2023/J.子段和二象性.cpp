15230fb9ddeebb6287471c07cb0893f54a68562f
gujiu6 <18273655445@163.com>
2026-08-04 21:11:02 +0800

    111

---
+++ CCPC/Province/XJCPC/2023/J.子段和二象性.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+//#include <ranges>
+using namespace std;
+#define ONLINE_JUDGE
+#define endl '\n'
+#define int long long
+#define all(A,i) A.begin()+i, A.end()
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define per(i,r,l) for(int i=r;i>=l;i--)
+#define pb push_back
+#define dbg(x) cout<<#x<<"="<<x<<endl;
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using cd = complex<double>;
+using pii = array<int, 2>;
+constexpr ll INF = 1e18;
+constexpr int MOD = 1e9+7;
+constexpr int MAXX = 2e5+10, LIMIT = 20;
+constexpr ld eps = 1e-6;
+const ld PI = acos(-1);
+constexpr int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
+constexpr int dx8[]={0,0,1,1,1,-1,-1,-1}, dy8[]={1,-1,-1,0,1,-1,0,1};
+
+
+
+
+
+
+inline void solve() {
+    int n, m; cin >> n >> m;
+    cout << (m - gcd(n, m) + n - 1) << endl;
+    
+}
+
+
+
+
+
+
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int t = 1;
+    cin >> t;
+    while(t--){
+    
+        solve();
+    
+    }
+    return 0;
+}
+// 难道我不配AC吗
+//   ▄████  █    ██  ▄▄▄██▀▀▀██▓ █    ██
+//  ██▒ ▀█▒ ██  ▓██▒   ▒██  ▓██▒ ██  ▓██▒
+// ▒██░▄▄▄░▓██  ▒██░   ░██  ▒██▒▓██  ▒██░
+// ░▓█  ██▓▓▓█  ░██░▓██▄██▓ ░██░▓▓█  ░██░
+// ░▒▓███▀▒▒▒█████▓  ▓███▒  ░██░▒▒█████▓
+//  ░▒   ▒ ░▒▓▒ ▒ ▒  ▒▓▒▒░  ░▓  ░▒▓▒ ▒ ▒
+//   ░   ░ ░░▒░ ░ ░  ▒ ░▒░   ▒ ░░░▒░ ░ ░
+//  ░   ░  ░░░ ░ ░  ░ ░ ░   ▒ ░ ░░░ ░ ░
+//       ░    ░      ░   ░   ░     ░
