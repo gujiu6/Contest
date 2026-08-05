73edf49fc2eb9dac44102a4a06a8790dc5b6004f
gujiu6 <18273655445@163.com>
2026-08-05 13:33:50 +0800

    111

---
+++ CodeForce/Div3/1114/2254B.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+//#include <ranges>
+using namespace std;
+#define ONLINE_JUDGE
+#define endl '\n'
+#define int long long
+#define all(A,i) A.begin()+i, A.end()
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define per(i,r,l) for(int i=r;i>=l;i--)
+#define pb push_back
+#define dbg(x) cout<<#x<<"="<<x<<endl;
+using i64 = long long;
+using i128 = __int128_t;
+using ld = long double;
+using ull = unsigned long long;
+using cd = complex<double>;
+using pii = array<int, 2>;
+constexpr i64 INF = 1e18;
+constexpr int MOD = 1e9+7;
+constexpr int MAXX = 2e5+10, LIMIT = 20;
+constexpr ld eps = 1e-6;
+const ld PI = acos(-1);
+constexpr int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
+constexpr int dx8[]={0,0,1,1,1,-1,-1,-1}, dy8[]={1,-1,-1,0,1,-1,0,1};
+
+
+
+
+
+
+inline void solve() {
+    int n; cin >> n;
+    string s; cin >> s; s = s;
+    if (n <= 1) {
+        cout << 0 << endl;
+        return;
+    }
+    int cnt = 0;
+    for(int i = 1; i < n; i++) {
+        if(s[i] != s[i - 1]) cnt++;
+    }
+    int ans = cnt + 1;
+    for(int i = 1; i < n - 1; i++) {
+        int l = (s[i - 1] != s[i]) + (s[i] != s[i + 1]);
+        int r = (s[i - 1] != s[i + 1]);
+        ans = min(ans, cnt - l + r + 1);
+    }
+    cout << ans << endl;
+    
+}
+
+
+
+
+
+
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int t = 1;
+    cin >> t;
+    while(t--){
+    
+        solve();
+    
+    }
+    return 0;
+}
+// 难道我不配AC吗
+//   ▄████  █    ██  ▄▄▄██▀▀▀██▓ █    ██
+//  ██▒ ▀█▒ ██  ▓██▒   ▒██  ▓██▒ ██  ▓██▒
+// ▒██░▄▄▄░▓██  ▒██░   ░██  ▒██▒▓██  ▒██░
+// ░▓█  ██▓▓▓█  ░██░▓██▄██▓ ░██░▓▓█  ░██░
+// ░▒▓███▀▒▒▒█████▓  ▓███▒  ░██░▒▒█████▓
+//  ░▒   ▒ ░▒▓▒ ▒ ▒  ▒▓▒▒░  ░▓  ░▒▓▒ ▒ ▒
+//   ░   ░ ░░▒░ ░ ░  ▒ ░▒░   ▒ ░░░▒░ ░ ░
+//  ░   ░  ░░░ ░ ░  ░ ░ ░   ▒ ░ ░░░ ░ ░
+//       ░    ░      ░   ░   ░     ░
---
+++ CodeForce/Div3/1114/2254E.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+//#include <ranges>
+using namespace std;
+#define ONLINE_JUDGE
+#define endl '\n'
+#define int long long
+#define all(A,i) A.begin()+i, A.end()
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define per(i,r,l) for(int i=r;i>=l;i--)
+#define pb push_back
+#define dbg(x) cout<<#x<<"="<<x<<endl;
+using i64 = long long;
+using i128 = __int128_t;
+using ld = long double;
+using ull = unsigned long long;
+using cd = complex<double>;
+using pii = array<int, 2>;
+constexpr i64 INF = 1e18;
+constexpr int MOD = 1e9+7;
+constexpr int MAXX = 2e5+10, LIMIT = 20;
+constexpr ld eps = 1e-6;
+const ld PI = acos(-1);
+constexpr int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
+constexpr int dx8[]={0,0,1,1,1,-1,-1,-1}, dy8[]={1,-1,-1,0,1,-1,0,1};
+
+
+
+
+
+
+inline void solve() {
+    int n; cin >> n;
+    vector<int> b(n + 1), a(n + 1);
+    multiset<int> st;
+    rep(i, 1, n) cin >> b[i], a[n] += b[i], st.insert(b[i]);
+    if(a[n] < 1) {
+        cout << -1 << endl;
+        return;
+    }
+    int sum = 0;
+    for(int i = 1; i < n; i++) {
+        auto it = st.lower_bound(1 - sum);
+        if(it == st.end()) {
+            cout << -1 << endl;
+            return;
+        }
+        a[i] = (*it) + a[i - 1];
+        sum += (*it);
+        st.erase(it);
+    }
+    for(int i = 1; i <= n; i++) cout << a[i] << " ";
+    cout << endl;
+    
+
+
+    
+}
+
+
+
+
+
+
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int t = 1;
+    cin >> t;
+    while(t--){
+    
+        solve();
+    
+    }
+    return 0;
+}
+// 难道我不配AC吗
+//   ▄████  █    ██  ▄▄▄██▀▀▀██▓ █    ██
+//  ██▒ ▀█▒ ██  ▓██▒   ▒██  ▓██▒ ██  ▓██▒
+// ▒██░▄▄▄░▓██  ▒██░   ░██  ▒██▒▓██  ▒██░
+// ░▓█  ██▓▓▓█  ░██░▓██▄██▓ ░██░▓▓█  ░██░
+// ░▒▓███▀▒▒▒█████▓  ▓███▒  ░██░▒▒█████▓
+//  ░▒   ▒ ░▒▓▒ ▒ ▒  ▒▓▒▒░  ░▓  ░▒▓▒ ▒ ▒
+//   ░   ░ ░░▒░ ░ ░  ▒ ░▒░   ▒ ░░░▒░ ░ ░
+//  ░   ░  ░░░ ░ ░  ░ ░ ░   ▒ ░ ░░░ ░ ░
+//       ░    ░      ░   ░   ░     ░
