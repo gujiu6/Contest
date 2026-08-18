a50b1e7f003032d587ad8b334ef3ad2450dead84
gujiu6 <18273655445@163.com>
2026-08-19 01:43:05 +0800

    11

---
+++ Province/Guangdong/2021/J.Jerry.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#include <cassert>
+//#include <ranges>
+using namespace std;
+#define ONLINE_JUDGE
+#define endl '\n'
+#define int long long
+#define all(A,i) A.begin()+i, A.end()
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define per(i,r,l) for(int i=r;i>=l;i--)
+#define dbg(x) cout<<#x<<"="<<x<<endl;
+using i64 = long long;
+using i128 = __int128_t;
+using ld = long double;
+using ui64 = unsigned long long;
+using cd = complex<double>;
+using pii = array<int, 2>;
+constexpr i64 INF = 1e18;
+constexpr int MOD = 1e9+7;
+constexpr int MAXX = 1e5, LIMIT = 20;
+constexpr ld eps = 1e-6;
+const ld PI = acos(-1);
+constexpr int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
+constexpr int dx8[]={0,0,1,1,1,-1,-1,-1}, dy8[]={1,-1,-1,0,1,-1,0,1};
+struct WEdge {int v;i64 w = 0;};struct DEdge {int u, v;i64 w = 0;};struct Edge {int v;};
+
+
+bool pd(int n) {
+    int x = sqrt(n);
+    return x * x == n;
+}
+
+
+
+inline void solve() {
+    int q; cin >> q;
+    while(q--) {
+        int d; cin >> d;
+        bool ok = 0;
+        if(pd(d)) {
+            cout << 1 << endl;
+            ok = 1;
+        }
+        if(ok) continue;
+        for(int i = 1; i * i <= d; i++) {
+            int a = i * i, b = d - a;
+            if(pd(b)) {
+                cout << 2 << endl;
+                ok = 1;
+                break;
+            }
+        }
+        if(ok) continue;
+        for(int i = 1; i * i + d<= MAXX; i++) {
+            int a = i * i, b = d + a;
+            if(pd(b)) {
+                cout << 2 << endl;
+                ok = 1;
+                break;
+            }
+        }
+        if(ok) continue;
+        cout << 3 << endl;
+    }
+    
+}
+
+
+
+
+
+
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int t = 1;
+    //cin >> t;
+    for(int i = 1; i <= t; i++){
+    
+        solve();
+    
+    }
+    return 0;
+}
+// 难道我不配AC吗
+//   ▄████  █    ██  ▄▄▄██▀▀▀██▓ █    ██
+//  ██▒ ▀█▒ ██  ▓██▒   ▒██  ▓██▒ ██  ▓██▒
+// ▒██░▄▄▄░▓██  ▒██░   ░██  ▒██▒▓██  ▒██░
+// ░▓█  ██▓▓▓█  ░██░▓██▄██▓ ░██░▓▓█  ░██░
+// ░▒▓███▀▒▒▒█████▓  ▓███▒  ░██░▒▒█████▓
+//  ░▒   ▒ ░▒▓▒ ▒ ▒  ▒▓▒▒░  ░▓  ░▒▓▒ ▒ ▒
+//   ░   ░ ░░▒░ ░ ░  ▒ ░▒░   ▒ ░░░▒░ ░ ░
+//  ░   ░  ░░░ ░ ░  ░ ░ ░   ▒ ░ ░░░ ░ ░
+//       ░    ░      ░   ░   ░     ░
