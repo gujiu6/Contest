895411054424e0be7281b37a1fb009533dd52dd1
gujiu6 <18273655445@163.com>
2026-08-20 17:49:58 +0800

    11

---
+++ Province/Guangxi/2025/G.Grand_XOR_Counting_Problem_Challenge_II.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#include <cassert>
+//#include <ranges>
+using namespace std;
+#define ONLINE_JUDGE
+#define endl '\n'
+// #define int long long
+#define all(A,i) A.begin()+i, A.end()
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define per(i,r,l) for(int i=r;i>=l;i--)
+#define dbg(x) cout<<#x<<"="<<x<<endl;
+using i64 = long long;
+using i128 = __int128;
+using ld = long double;
+using u64 = unsigned long long;
+using cd = complex<double>;
+using pii = array<int, 2>;
+constexpr i64 INF = 1e18;
+constexpr int MOD = 1e9+7;
+constexpr int MAXX = 2e5+10, LIMIT = 20;
+constexpr ld eps = 1e-6;
+const ld PI = acos(-1);
+constexpr int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
+constexpr int dx8[]={0,0,1,1,1,-1,-1,-1}, dy8[]={1,-1,-1,0,1,-1,0,1};
+struct WEdge {int v;i64 w = 0;};struct DEdge {int u, v;i64 w = 0;};struct Edge {int v;};
+
+
+
+
+inline void solve() {
+    int n, k1, k2; cin >> n >> k1 >> k2;
+    vector<int> c(n + 1), a(n + 1), b(n + 1);
+    int k = k1 ^ k2;
+    i64 ans = 0;
+    rep(i, 1, n) cin >> a[i];
+    rep(i, 1, n) cin >> b[i];
+    rep(i, 1, n) c[i] = a[i] ^ b[i];
+    unordered_map<u64, int> t;
+    rep(p, 0, 29) {
+        t.clear();
+        t.reserve(n);
+        rep(j, 1, n) {
+            int high_bit = ((k ^ c[j]) >> p) ^ 1;
+            int cur_bit_a = (k1 ^ a[j]) >> p & 1;
+            u64 tar_x = (high_bit << 1) | cur_bit_a;
+            u64 cur_x = ((c[j] >> p) << 1) | ((a[j] >> p) & 1);
+            auto it = t.find(tar_x);
+            if(it != t.end()) ans += it->second;
+            t[cur_x]++;
+        }
+    }
+    cout << ans << endl;
+    
+}
+
+
+
+
+
+
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int t = 1;
+    //cin >> t;
+    for(int i = 1; i <= t; i++){
+    
+        solve();
+    
+    }
+    return 0;
+}
+// 难道我不配AC吗
+//   ▄████  █    ██  ▄▄▄██▀▀▀██▓ █    ██
+//  ██▒ ▀█▒ ██  ▓██▒   ▒██  ▓██▒ ██  ▓██▒
+// ▒██░▄▄▄░▓██  ▒██░   ░██  ▒██▒▓██  ▒██░
+// ░▓█  ██▓▓▓█  ░██░▓██▄██▓ ░██░▓▓█  ░██░
+// ░▒▓███▀▒▒▒█████▓  ▓███▒  ░██░▒▒█████▓
+//  ░▒   ▒ ░▒▓▒ ▒ ▒  ▒▓▒▒░  ░▓  ░▒▓▒ ▒ ▒
+//   ░   ░ ░░▒░ ░ ░  ▒ ░▒░   ▒ ░░░▒░ ░ ░
+//  ░   ░  ░░░ ░ ░  ░ ░ ░   ▒ ░ ░░░ ░ ░
+//       ░    ░      ░   ░   ░     ░
