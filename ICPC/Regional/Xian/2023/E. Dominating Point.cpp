0fa67384de11e6e825ff13a57c2da247b322fd3a
gujiu6 <18273655445@163.com>
2026-09-15 09:03:03 +0800

    11

---
+++ ICPC/Regional/Xian/2023/E. Dominating Point.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+#include <cassert>
+//#include <ranges>
+using namespace std;
+#define ONLINE_JUDGE
+#define endl '\n'
+#define int long long
+#define all(A,i) A.begin()+i, A.end()
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define per(i,r,l) for(int i=r;i>=l;i--)
+#define dbg(x) cout<<#x<<"="<<x<<endl;
+using i64 = long long;
+using i128 = __int128;
+using ld = long double;
+using u64 = unsigned long long;
+using cd = complex<double>;
+using pii = array<int, 2>;
+constexpr i64 INF = 1e18;
+constexpr int MOD = 1e9+7;
+constexpr int MAXX = 2e5+10, LIMIT = 20;
+constexpr ld eps = 1e-6;
+const ld PI = acos(-1);
+constexpr int dx[]={0,0,1,-1}, dy[]={1,-1,0,0};
+constexpr int dx8[]={0,0,1,1,1,-1,-1,-1}, dy8[]={1,-1,-1,0,1,-1,0,1};
+struct WEdge {int v;i64 w = 0;};struct DEdge {int u, v;i64 w = 0;};struct Edge {int v;};
+
+
+
+
+
+inline void solve() {
+    int n; cin >> n;
+    vector<vector<Edge>> g(n + 1);
+    vector<int> out(n + 1);
+    rep(u, 1, n) {
+        rep(v, 1, n) {
+            char c; cin >> c;
+            if(c == '1') {
+                g[u].push_back({v});
+                out[u]++;
+            }
+        }
+    }
+    auto calc = [&](int u) {
+        int x = 0;
+        vector<int> vis(n + 1);
+        vis[u] = 1;
+        for(auto &e : g[u]) {
+            vis[e.v] = 1;
+        }
+        for(int i = 1; i <= n; i++) {
+            if(vis[i]) continue;
+            x = out[i] > out[x] ? i : x;
+        }
+        return x;
+    };
+    int a = calc(0);
+    if(a > 0) {
+        int b = calc(a);
+        if(b > 0) {
+            int c = calc(b);
+            if(c > 0) {
+                cout << a << " " << b << " " << c << endl;
+                return;
+            }
+        }
+    }
+    cout << "NOT FOUND" << endl;
+
+    
+}
+
+
+
+
+
+
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int t = 1;
+    //cin >> t;
+    for(int i = 1; i <= t; i++){
+        
+        solve();
+    
+    }
+    return 0;
+}
+// 难道我不配AC吗
+//   ▄████  █    ██  ▄▄▄██▀▀▀██▓ █    ██
+//  ██▒ ▀█▒ ██  ▓██▒   ▒██  ▓██▒ ██  ▓██▒
+// ▒██░▄▄▄░▓██  ▒██░   ░██  ▒██▒▓██  ▒██░
+// ░▓█  ██▓▓▓█  ░██░▓██▄██▓ ░██░▓▓█  ░██░
+// ░▒▓███▀▒▒▒█████▓  ▓███▒  ░██░▒▒█████▓
+//  ░▒   ▒ ░▒▓▒ ▒ ▒  ▒▓▒▒░  ░▓  ░▒▓▒ ▒ ▒
+//   ░   ░ ░░▒░ ░ ░  ▒ ░▒░   ▒ ░░░▒░ ░ ░
+//  ░   ░  ░░░ ░ ░  ░ ░ ░   ▒ ░ ░░░ ░ ░
+//       ░    ░      ░   ░   ░     ░
