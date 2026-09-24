185eef7e75ca58039fd6e83727ba2e612b3be46b
故旧 <18273655445@163.com>
2026-09-24 22:36:07 +0800

    11

--- Other/My/Hengyang2026New/B/gen.cpp
+++ Other/My/Hengyang2026New/B/gen.cpp
@@ -1,24 +1,33 @@
 #include "testlib.h"
-#include <bits/stdc++.h>
-#define endl '\n'
+#include <iostream>
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
     registerGen(argc, argv, 1);
-    int l = 1, r = 5000000;
+    
+    int test_id = opt<int>(1, 1);
 
-    // 参数：
-    // gen l r
-    if (argc >= 3) {
-        l = stoi(argv[1]);
-        r = stoi(argv[2]);
+    int l = 1, r = 5000000;
+    if (test_id <= 10) {
+        l = 1;
+        r = test_id;
+    } else if (test_id <= 30) {
+        l = 100;
+        r = 100000;
+    } else if (test_id <= 50) {
+        l = 1000000;
+        r = 4000000;
+    } else {
+        l = 4000000;
+        r = 5000000;
     }
 
-    ensuref(1 <= l && l <= 5000000, "Invalid l range");
-    ensuref(l <= r && r <= 5000000, "Invalid r range");
+    ensuref(1 <= l && l <= 5000000, "Invalid l range: %d", l);
+    ensuref(l <= r && r <= 5000000, "Invalid r range: %d", r);
 
     int n = rnd.next(l, r);
-    cout << n << endl;
+    cout << n << "\n";
 
     return 0;
 }
